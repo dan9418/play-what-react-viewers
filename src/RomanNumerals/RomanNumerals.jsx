546af22c42ac9099fa71fd0697b99579cc1cef15
Dan Bednarczyk <dpbednarczyk@gmail.com>
2019-12-19 10:14:47 -0800

    work on RNs

--- src/RomanNumerals/RomanNumerals.jsx
+++ src/RomanNumerals/RomanNumerals.jsx
@@ -15,29 +15,47 @@ function IntervalLabel(props) {
     )
 }
 
+function RomanNumeralBlock(props) {
+    return (
+        <div
+            className='roman-numeral-block'
+        >
+            <div>{props.romanNumeral.name}</div>
+            {getIntervalLabels(props.romanNumeral.intervals)}
+            {getIntervalLabels(props.romanNumeral.relativeIntervals)}
+        </div>
+    )
+}
+
+function getIntervalLabels(intervals) {
+    return <div>{intervals.map((i) => <IntervalLabel key={i.id} interval={i} />)}</div>;
+}
+
 export default function RomanNumerals(props) {
     let config = Object.assign({}, DEFAULT_PROPS, props);
 
     if (!config.concept || (!(config.concept instanceof Theory.ConceptTypes.HeptatonicScale) && !(config.concept instanceof Theory.ConceptTypes.RomanNumeral)))
         return null;
 
-    let keyCenterIntervals = config.concept.intervals;
+    let conceptIntervals = config.concept.intervals;
+    //let keyCenterIntervals = config.concept.intervals;
     //let relativeIntervals = props.romanNumeral.relativeIntervals;
     return (
         <div className='roman-numerals'>
             {config.concept.name}
-            
+
             <div>
-                {keyCenterIntervals.map((i) =>
-                    <IntervalLabel key={i.id} interval={i} />
-                )}
+                {getIntervalLabels(conceptIntervals)}
             </div>
-            {/*<div>
-                {relativeIntervals.map((i) =>
-                    <IntervalLabel key={i.id} interval={i} />
-                )}
+            <div>
+                {
+                    conceptIntervals.map((ci) => {
+                        let degree = ci.degree;
+                        let romanNumeral = new Theory.ConceptTypes.RomanNumeral(config.concept, degree);
+                        return <RomanNumeralBlock key={degree} romanNumeral={romanNumeral} />;
+                    })
+                }
             </div>
-            */}
         </div>
     );
 }
