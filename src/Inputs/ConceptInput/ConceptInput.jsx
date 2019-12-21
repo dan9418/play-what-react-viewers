a96b52c54becf3cda6d3e7bda19d7a8637a4e9a3
Dan Bednarczyk <dpbednarczyk@gmail.com>
2019-12-20 18:04:06 -0800

    add concept interactive

--- src/Inputs/ConceptInput/ConceptInput.jsx
+++ src/Inputs/ConceptInput/ConceptInput.jsx
@@ -1,11 +1,66 @@
-import * as React from 'react';
+import React from 'react';
 import './ConceptInput.css';
+import DropdownInput from '../DropdownInput/DropdownInput';
+import { Theory } from 'play-what';
 
-export default function ConceptInput(props) {
-    return (
-        <div
-            className='concept-innput'
-        >
-        </div>
-    );
+const PRESET_TYPES = [
+    {
+        id: 'intervalPair',
+        name: 'Interval Pair',
+        presets: Object.values(Theory.Presets.INTERVAL_PAIR)
+    },
+    {
+        id: 'chord',
+        name: 'Chord',
+        presets: Object.values(Theory.Presets.CHORD)
+    },
+    {
+        id: 'scale',
+        name: 'Scale',
+        presets: Object.values(Theory.Presets.SCALE)
+    },
+    {
+        id: 'mode',
+        name: 'Mode',
+        presets: Object.values(Theory.Presets.MODE)
+    }
+];
+
+export default class ConceptInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedType: PRESET_TYPES[0],
+            selectedPreset: PRESET_TYPES[0].presets[0]
+        }
+    }
+    render() {
+        return (
+            <div
+                className='concept-input'
+            >
+                <div className='input-row'>
+                    <label>Preset Type:</label>
+                    <DropdownInput
+                        data={PRESET_TYPES}
+                        value={this.state.selectedType}
+                        setValue={(type) => this.setState({ selectedType: type })}
+                    />
+                </div>
+
+                <div className='input-row'>
+                    <label>{this.state.selectedType.name}:</label>
+                    <DropdownInput
+                        data={this.state.selectedType.presets}
+                        value={this.state.selectedPreset}
+                        setValue={(preset) => {
+                            // Not sure if safe to update both states like this...
+                            this.setState({ selectedPreset: preset });
+                            this.props.setConcept(preset);
+                        }}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
