a9ed112d82082a1f63c5c83e006891f05a5276a2
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-01-02 15:03:23 -0800

    remove old view controller

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import Inputs from "./Inputs/_module";
 
-import ViewController from "./ViewController/ViewController";
-
 import Fretboard from "./Fretboard/_module";
 import Keyboard from "./Keyboard/_module";
 
@@ -9,6 +7,4 @@ import ChordAnalysis from "./ChordAnalysis/ChordAnalysis";
 import ConceptBlock from "./ConceptBlock/ConceptBlock";
 import NoteTable from "./NoteTable/NoteTable";
 
-
-
-export { Fretboard, Keyboard, Inputs, ViewController, ChordAnalysis, ConceptBlock, NoteTable };
+export { Fretboard, Keyboard, Inputs, ChordAnalysis, ConceptBlock, NoteTable };
