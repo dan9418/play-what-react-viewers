6c77889ed85285571b7dc7bab146e557d62929f6
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-02-09 15:31:42 -0800

    cleanups

--- src/Fretboard/_module.js
+++ src/Fretboard/_module.js
@@ -1,5 +1,4 @@
 import Viewer from './Fretboard';
 import Defaults from './Fretboard.defaults';
-import * as Strategies from './Fretboard.strategies';
 
-export default { Viewer, Defaults, Strategies };
+export default { Viewer, Defaults };
--- src/Keyboard/_module.js
+++ src/Keyboard/_module.js
@@ -1,5 +1,4 @@
 import Viewer from './Keyboard';
 import Defaults from './Keyboard.defaults';
-import * as Strategies from './Keyboard.strategies';
 
-export default { Viewer, Defaults, Strategies };
+export default { Viewer, Defaults };
