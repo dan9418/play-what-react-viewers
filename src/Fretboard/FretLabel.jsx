2ea2fd0697202db146683f1a2c671958d49a3438
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-03-05 21:44:04 -0800

    set up testing code

--- src/Fretboard/FretLabel.jsx
+++ src/Fretboard/FretLabel.jsx
@@ -9,7 +9,7 @@ function callConfigFunction(configFunction, ...args) {
 }
 
 export default function FretLabel(props) {
-    let config = Object.assign({}, DEFAULT_PROPS, props, props.labelProps);
+    let config = { ...DEFAULT_PROPS, ...props };
 
     let note = callConfigFunction(config.mapStrategy, config.noteIndex, config.keyCenter, config.concept);
     note = note ? note : new Theory.NonfunctionalNote(config.noteIndex);
@@ -18,9 +18,40 @@ export default function FretLabel(props) {
     let label = callConfigFunction(config.labelStrategy, note);
     let action = callConfigFunction(config.actionStrategy, note);
 
+    let scaleTone = callConfigFunction(config.mapStrategy, config.noteIndex, new PlayWhat.KeyCenter(PlayWhat.Constants.TONIC.G, PlayWhat.Constants.ACCIDENTAL.Natural, 4), PlayWhat.Presets.SCALE.Major);
+
+    console.log(scaleTone);
+
+    styles = {
+        color: 'white',
+        backgroundColor: getBG(note.interval && note.interval.degree, scaleTone.interval)
+    };
+
     return (
         <div className='fret-label' style={styles} onClick={action}>
             {label}
         </div>
     );
+}
+
+var getBG = (degree, scaleTone) => {
+    const def = '#999';
+    switch (degree) {
+        case 1:
+            return 'red';
+        case 2:
+            return def;
+        case 3:
+            return 'green';
+        case 4:
+            return def;
+        case 5:
+            return def;
+        case 6:
+            return def;
+        case 7:
+            return 'blue';
+        default:
+            return scaleTone ? '#DDD' : '';
+    }
 }
