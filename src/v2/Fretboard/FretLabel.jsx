e958a0f3614067542fd4111a3a3db8b8157d6f67
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-04-08 19:57:43 -0700

    color

--- src/v2/Fretboard/FretLabel.jsx
+++ src/v2/Fretboard/FretLabel.jsx
@@ -5,10 +5,11 @@ import "./Fretboard.css";
 const FretLabel = props => {
     const { notes, noteIndex } = props;
 
-    const note = PW.v2.Theory.getNoteByPitchClass(notes, noteIndex);
+    const note = PW.v2.Theory.getNoteByPitchClass(notes, PW.v2.Utils.modulo(noteIndex, 12));
+    const styles = PW.v2.Color.byDegree(note);
 
     return (
-        <div className='fret-label' style={{}} onClick={() => null}>
+        <div className='fret-label' style={styles} onClick={() => null}>
             {note && note.name}
         </div>
     );
