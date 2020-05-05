ca66ba0c485411ce704aacb8c6c412eb27c7e43e
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-05-04 17:42:08 -0700

    two modes

--- src/Modules/Graph/GraphCard.jsx
+++ src/Modules/Graph/GraphCard.jsx
@@ -3,6 +3,7 @@ import PW from 'play-what';
 
 import Graph from './Graph';
 import ConceptInput from '../../UI/ConceptInput/ConceptInput';
+import VectorsInput from '../../UI/VectorsInput/VectorsInput';
 
 const MAX_VECTOR = PW.Constants.MAX_VECTOR;
 const ORIGIN = { p: 0, d: 2 };
@@ -20,12 +21,15 @@ const GraphCard = () => {
     const [max, setMax] = useState(MAX_VECTOR);
     const [origin, setOrigin] = useState(ORIGIN);
     const [vectors, setVectors] = useState(VECTORS);
+    const [mathMode, setMathMode] = useState(false);
 
     return (
         <Card title="Graph" defaultOpen>
+            <div className="mode-button" onClick={() => setMathMode(!mathMode)}>Toggle Mode</div>
             <div className="graph-card">
                 <div className='left'>
-                    <ConceptInput keyCenter={origin} setKeyCenter={setOrigin} intervals={vectors} setIntervals={setVectors} max={max} />
+                    {!mathMode && <ConceptInput keyCenter={origin} setKeyCenter={setOrigin} intervals={vectors} setIntervals={setVectors} max={max} />}
+                    {mathMode && <VectorsInput keyCenter={origin} setKeyCenter={setOrigin} intervals={vectors} setIntervals={setVectors} max={max} />}
                 </div>
                 <div className='right'>
                     <Graph origin={origin} vectors={vectors} x='d' y='p' max={max} title="d vs p" yLabel="p" xLabel="d" />
--- src/UI/VectorsInput/VectorsInput.jsx
+++ src/UI/VectorsInput/VectorsInput.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import './ConceptInput.css';
-
-import PresetInput from './PresetInput'
+import './VectorsInput.css';
 
 import PW from 'play-what';
+
+const VectorInput = ({ value, setValue }) => {
+    const { p, d } = value;
+    return (
+        <div className='vector-input'>
+            <input type='number' disabled={!setValue} value={d} onChange={e => setValue({ d: parseInt(e.target.value), p: p })} />
+            <input type='number' disabled={!setValue} value={p} onChange={e => setValue({ d: d, p: parseInt(e.target.value) })} />
+        </div>
+    );
+};
 
 const IntervalLabel = ({ value, max }) => {
     return (
@@ -25,7 +33,9 @@ const VectorRow = ({ vector, origin, max, setValue }) => {
     const resultant = PW.Theory.addVectors(origin, vector, max);
     return (
         <div className='vector-row'>
+            <VectorInput value={vector} setValue={setValue} />
             <IntervalLabel value={vector} max={max} />
+            <VectorInput value={resultant} />
             <NoteLabel value={resultant} max={max} />
         </div>
     );
@@ -42,11 +52,11 @@ const ConceptInput = props => {
         <div className="concept-input">
             <div className='origin-input'>
                 <label>Key Center</label>
+                <VectorInput value={keyCenter} setValue={setKeyCenter} point />
                 <NoteLabel value={keyCenter} max={max} />
             </div>
             <div className='vector-rows'>
                 <label>Intervals</label>
-                <PresetInput value={intervals} setValue={setIntervals} />
                 {rows}
             </div>
         </div>
