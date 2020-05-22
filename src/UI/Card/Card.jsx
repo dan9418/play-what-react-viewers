eb4bb0743856c64c7bbc7b739f6dd0b0af6492df
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-05-21 21:16:33 -0700

    card

--- src/UI/Card/Card.jsx
+++ src/UI/Card/Card.jsx
@@ -5,8 +5,8 @@ const Card = ({ title, children, defaultOpen }) => {
     const [open, setOpen] = useState(defaultOpen || true);
 
     return (
-        <div className={`card ${open ? 'open' : ''}`}>
-            <h2 className="title" onClick={() => setOpen(!open)}>{title}</h2>
+        <div className={`card pw-light ${open ? 'open' : ''}`}>
+            <div className="title pw-secondary pw-hov" onClick={() => setOpen(!open)}>{title}</div>
             {open && <div className="content ">{children}</div>}
         </div>
     )
