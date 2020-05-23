c5e396ac76d7957dad32a53099d4e1186be337e1
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-05-23 07:30:18 -0700

    fretboard

--- src/Modules/Fretboard/FretboardCard.jsx
+++ src/Modules/Fretboard/FretboardCard.jsx
@@ -6,6 +6,7 @@ import Fretboard from './Fretboard';
 import ConceptInput from '../../UI/ConceptInput/ConceptInput';
 import ScalarInput from '../../UI/ScalarInput/ScalerInput';
 import SwitchInput from '../../UI/SwitchInput/SwitchInput';
+import InputBlock from '../../UI/InputBlock/InputBlock';
 
 const FretboardCard = ({ defaultOpen }) => {
     const [keyCenter, setKeyCenter] = useState(PW.Presets.KEY_CENTERS.A);
@@ -29,14 +30,18 @@ const FretboardCard = ({ defaultOpen }) => {
 
             <ConceptInput keyCenter={keyCenter} setKeyCenter={setKeyCenter} intervals={intervals} setIntervals={setIntervals} />
 
-            <label>Low Fret:</label>
-            <ScalarInput value={fretLow} setValue={setFretLow} />
-            <label>High Fret:</label>
-            <ScalarInput value={fretHigh} setValue={setFretHigh} />
-            <label>Show Dots:</label>
-            <SwitchInput value={showDots} setValue={setShowDots} />
-            <label>Show Fret Numbers:</label>
-            <SwitchInput value={showFretNumbers} setValue={setShowFretNumbers} />
+            <InputBlock title="Fret Range">
+                <label>Low Fret:</label>
+                <ScalarInput value={fretLow} setValue={setFretLow} />
+                <label>High Fret:</label>
+                <ScalarInput value={fretHigh} setValue={setFretHigh} />
+            </InputBlock>
+            <InputBlock title="Labels">
+                <label>Show Dots:</label>
+                <SwitchInput value={showDots} setValue={setShowDots} />
+                <label>Show Fret Numbers:</label>
+                <SwitchInput value={showFretNumbers} setValue={setShowFretNumbers} />
+            </InputBlock>
         </Card>
     )
 }
