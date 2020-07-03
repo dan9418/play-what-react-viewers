2b26a3c67a26a1285015197cad77371652fabba3
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-07-02 20:20:25 -0700

    navigation

--- src/Modules/Chart/Chart.jsx
+++ src/Modules/Chart/Chart.jsx
@@ -7,7 +7,7 @@ const DEFAULT_COL = { a: PW.Presets.KEY_CENTERS.C, B: PW.Presets.QUICK_MODE.Ioni
 const DEFAULT_ROW = [DEFAULT_COL];
 
 const Col = props => {
-    const { a, B, t, sectionIndex, rowIndex, colIndex } = props;
+    const { a, B, t, sectionIndex, rowIndex, colIndex, setPosition, position } = props;
     // const notes = PW.Theory.addVectorsBatch(a, B);
 
     const tonic = PW.Theory.getNoteName(a);
@@ -16,9 +16,9 @@ const Col = props => {
 
     const style = { flexGrow: t };
 
-    const { sectionIndex: s, rowIndex: r, colIndex: c, setPosition, setT } = { sectionIndex: 0, rowIndex: 0, colIndex: 0, setPosition: () => null, setT: () => null };
+    const [s, r, c] = position;
     const isActive = sectionIndex === s && rowIndex === r && colIndex === c;
-    const setPositionToThis = () => setPosition(sectionIndex, rowIndex, colIndex);
+    const setPositionToThis = () => setPosition([sectionIndex, rowIndex, colIndex]);
 
     return (
         <div className={`col ${isActive ? 'pw-accent' : 'pw-lighter'}`} style={style} onClick={setPositionToThis}>
@@ -27,47 +27,35 @@ const Col = props => {
                 <span className="preset">{preset.id}</span>
             </div>
             <div className="beats">
-                <ScalarInput value={t} setValue={setT} />
+                <ScalarInput value={t} setValue={() => null} />
             </div>
         </div>
     );
 };
 
-const getCols = (cols, sectionIndex, rowIndex) => {
-    return cols.map((c, i) => <Col key={i} sectionIndex={sectionIndex} rowIndex={rowIndex} colIndex={i} a={c.a} B={c.B} t={c.t} />);
-};
-
 const Row = props => {
-    const { cols, sectionIndex, rowIndex } = props;
+    const { cols, sectionIndex, rowIndex, setPosition, position } = props;
     return (
         <div className={`row ${null}`}>
-            {getCols(cols, sectionIndex, rowIndex)}
+            {cols.map((c, i) => <Col key={i} sectionIndex={sectionIndex} rowIndex={rowIndex} colIndex={i} a={c.a} B={c.B} t={c.t} setPosition={setPosition} position={position} />)}
         </div>
     );
-};
-
-const getRows = (rows, sectionIndex) => {
-    return rows.map((r, i) => <Row key={i} sectionIndex={sectionIndex} rowIndex={i} cols={r.cols} />);
 };
 
 const Section = props => {
-    const { name, rows, sectionIndex } = props;
+    const { name, rows, sectionIndex, setPosition, position } = props;
     return (
         <div className={`section ${null}`}>
             <div className="name">{name}</div>
-            {getRows(rows, sectionIndex)}
+            {rows.map((r, i) => <Row key={i} sectionIndex={sectionIndex} rowIndex={i} cols={r.cols} setPosition={setPosition} position={position} />)}
         </div>
     );
 };
 
-const getSections = sections => {
-    return sections.map((s, i) => <Section key={i} sectionIndex={i} name={s.name} rows={s.rows} />);
-};
-
-const Chart = ({ source }) => {
+const Chart = ({ source, position, setPosition }) => {
     return (
         <div className="chart">
-            {getSections(source.sections)}
+            {source.sections.map((s, i) => <Section key={i} sectionIndex={i} name={s.name} rows={s.rows} setPosition={setPosition} position={position} />)}
         </div>
     );
 }
