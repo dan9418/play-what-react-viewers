6030fd28ff2fb2a05b3a49c22f778415b73fda49
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-08-19 19:04:41 -0700

    fix

--- src/Fretboard/Fretboard.defaults.js
+++ src/Fretboard/Fretboard.defaults.js
@@ -1,4 +1,6 @@
 export default {
+    fretRange: [0, 12],
+    tuning: [16, 11, 7, 2, -3, 8], // e B G D A E
     fretMap: [
         [null, { children: 'test', style: { color: 'red' } }, null, null, null, null, null, null, null, null, null, null, null],
         [null, null, null, null, null, null, null, null, null, null, null, null, null],
@@ -8,11 +10,3 @@ export default {
         [null, null, null, null, null, null, null, null, null, null, null, null, null]
     ]
 };
-
-/*
-showFretNumbers: true,
-    showDots: true,
-    fretLow: 0,
-    fretHigh: 12,
-    tuning: [16, 11, 7, 2, -3, 8], // e B G D A E
-*/
