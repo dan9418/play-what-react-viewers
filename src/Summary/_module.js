7b41b7c85368df17d16f24497ea26fc407df4257
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-08-23 16:06:34 -0700

    sumary

--- src/Summary/_module.js
+++ src/Summary/_module.js
@@ -1,5 +1,19 @@
-import Viewer from './Summary';
+import Component from './Summary';
 
 export default {
-    Viewer
+    component: Component,
+    colorBy: props => {
+        /*return ctx => {
+            const { tuning, stringIndex, fretRange, fretIndex, noteIndex, note } = ctx;
+            const color = parseColorProp(props.type, note);
+            return color;
+        };*/
+    },
+    textBy: props => {
+        /*return ctx => {
+            const { tuning, stringIndex, fretRange, fretIndex, noteIndex, note } = ctx;
+            const text = parseTextProp(props.type, note);
+            return text;
+        };*/
+    }
 };
--- src/api.js
+++ src/api.js
@@ -1,52 +1,8 @@
 import _Fretboard from './Fretboard/_module';
-import _Grid from './Grid/_module';
 import _Summary from './Summary/_module';
 
 export const Fretboard = _Fretboard;
-
-export const Row = {
-    component: _Grid.Row.Viewer,
-    from: (args) =>
-        // const config = { ...Fretboard.Defaults, ...args };
-        // const { fretRange, tuning, labelFn, styleFn } = config;
-        ({
-            component: _Grid.Row.Viewer,
-            props: {
-                // fretMap: strings
-            }
-        })
-};
-
-const buildNoteSummary = (pod) => {
-    const name = '';//Note.getName({ pod });
-    const { p, d } = pod;
-    return {
-        name,
-        vector: [p, d]
-    };
-};
-
-export const Summary = {
-    component: _Summary.Viewer,
-    from: ({ pods }) => {
-        return {
-            component: _Summary.Viewer,
-            props: {
-                notes: pods.map((p) => buildNoteSummary(p))
-            }
-        };
-    }
-};
-
-export const repeat = ({ n, content }) => {
-    const result = [];
-    for (let i = 0; i < n; i++) {
-        // result.push(Source.parse(content));
-    }
-    return {
-        children: result
-    };
-};
+export const Summary = _Summary;
 
 export const element = {
     from: ({ component, children, props }) => {
