a88467b8dc2092e2f599c5fa9de6e9a5df6dd32d
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-08-24 08:34:41 -0700

    for each

--- src/Summary/_module.js
+++ src/Summary/_module.js
@@ -1,8 +1,17 @@
+import React from 'react'
 import Component from './Summary';
 import PW from 'play-what';
 
 export default {
     component: Component,
+    forEachNote: ({ concepts, props }) => {
+        return concepts.map(n => {
+            return {
+                component: Component,
+                props: { keyCenter: n.a, intervals: n.B, ...props }
+            };
+        })
+    },
     colorBy: props => {
         return ctx => {
             const { note } = ctx;
