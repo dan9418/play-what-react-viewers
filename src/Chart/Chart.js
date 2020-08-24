d73184a4cd7509a6304f64dba1212efbb1ddc122
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-08-24 16:16:59 -0700

    multi

--- src/Chart/Chart.js
+++ src/Chart/Chart.js
@@ -1,15 +1,36 @@
 import * as React from "react";
 //import "./Keyboard.css";
 
-const Chart = ({ children, ...props }) => {
+const Chart = ({ children, chart, viewer, ...props }) => {
 
     const rowStyles = {
         width: '100%'
     };
 
+    const sections = chart.sections.map(s => {
+        const concepts = s.concepts.map(c => {
+            const Comp = viewer.component;
+            const { a: keyCenter, B: intervals } = c;
+            return (
+                <div className="concept">
+                    <h4>Concept</h4>
+                    <Comp {...viewer.props} keyCenter={keyCenter} intervals={intervals} />
+                </div>
+            );
+        })
+        return (
+            <div className="section">
+                <h3>Section</h3>
+                <div className="concept-list">
+                    {concepts}
+                </div>
+            </div>
+        );
+    })
+
     return (
         <div className='chart' style={rowStyles}>
-            {children}.
+            {sections}
         </div>
     );
 }
