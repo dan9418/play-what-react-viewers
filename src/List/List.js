0c2c7d1226cb1968fa421d7e9404de5b97e57b76
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-08-24 17:18:55 -0700

    fix

---
+++ src/List/List.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+//import "./Keyboard.css";
+
+const Chart = ({ children, list, viewer, ...props }) => {
+
+    const rowStyles = {
+        width: '100%'
+    };
+
+    const concepts = list.map(c => {
+        const Comp = viewer.component;
+        const { a: keyCenter, B: intervals } = c;
+        return (
+            <div className="concept">
+                <h4>Concept</h4>
+                <Comp {...viewer.props} keyCenter={keyCenter} intervals={intervals} />
+            </div>
+        );
+    });
+
+    return (<div className="list">
+        {concepts}
+    </div>
+    );
+}
+
+export default Chart;
--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 import _Fretboard from './Fretboard/_module';
 import _Summary from './Summary/_module';
 import _Chart from './Chart/_module';
+import _List from './List/_module';
 
 export const Fretboard = _Fretboard;
 export const Summary = _Summary;
 export const Chart = _Chart;
+export const List = _List;
 
 export const element = {
     from: ({ component, children, props }) => {
